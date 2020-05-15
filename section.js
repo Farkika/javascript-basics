9b88f1c2b977677252bb3c3c06fefec875f2357e
Dani <farkika39@gmail.com>
2020-05-16 00:01:12 +0200

    adding section.js

---
+++ section.js
@@ -0,0 +1,9 @@
+const section = (arr1, arr2) => {
+  const section = new Array();
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr2.includes(arr1[i])) {
+      union.push(arr1[i]);
+    }
+  }
+  return section;
+};
