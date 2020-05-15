64375fb28619f034ab8f4c5c91f0304677e8de79
Dani <farkika39@gmail.com>
2020-05-16 00:01:40 +0200

    adding select.js

---
+++ select.js
@@ -0,0 +1,9 @@
+const select = (arr) => {
+  const newArr = new Array();
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === condition) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+};
